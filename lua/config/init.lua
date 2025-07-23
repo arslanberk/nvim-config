99bbe54722f83aeccba6797d1c92918b228f6a5e
Berk Arslan <arslanberk@Berks-Mac-Studio.local>
2025-07-23 05:27:04 +0300

    initial commit

---
+++ lua/config/init.lua
@@ -0,0 +1,13 @@
+require 'config.global'
+require 'config.remap'
+require 'config.lazy'
+
+vim.cmd 'colorscheme habamax'
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
